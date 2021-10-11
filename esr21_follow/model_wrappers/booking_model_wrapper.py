963b09b64a589dca2af2d0cd50152280f9bac288
ame diphoko <diphokoame@gmail.com>
2021-10-11 12:52:06 +0200

    update call log entry model admin

--- esr21_follow/model_wrappers/booking_model_wrapper.py
+++ esr21_follow/model_wrappers/booking_model_wrapper.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from edc_model_wrapper import ModelWrapper
 
 
-
 class BookingModelWrapper(ModelWrapper):
 
     model = 'esr21_follow.booking'
@@ -11,5 +10,3 @@ class BookingModelWrapper(ModelWrapper):
     next_url_attrs = ['subject_cell']
     next_url_name = settings.DASHBOARD_URL_NAMES.get(
         'esr21_follow_booking_listboard_url')
-
-
